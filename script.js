bf671c03eeb128cbddefc8f16867bfb92615c777
Igor Arecippo <iarecippo@gmail.com>
2022-04-25 17:09:16 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const selecionado = document.getElementById('cor1');
-
+const selecionado = document.getElementById('black');
 if (selecionado.classList) selecionado.classList.add ('selected');
-else selecionado.className += ' selected';
+else selecionado.className += ' selected'
+
+let cores = document.querySelectorAll('.color');
+        for (let i = 0; i < cores.length; i += 1) {
+            cores[i].addEventListener('click', mudaClasse);
+    }
+
+    function mudaClasse (event) {
+    let corSelecionada = document.querySelector('.selected');
+    corSelecionada.classList.remove('selected');
+    event.target.classList.add('selected');
+
+
+    
+}
+
+
+/*
+function mudaCor (event) {
+    let pixelSelecionado = document.querySelector('.pixel')
+    event.target.classList.add('selected');
+
+
+let pixels = document.querySelectorAll('.pixel')
+    for (i in pixels)
+    pixelsaddEventListener('click', mudaCor)
+
+
+}*/
