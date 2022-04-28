08875eb9fb9dbdd643d0d15737d365f219b6e88a
Igor Arecippo <iarecippo@gmail.com>
2022-04-28 17:41:52 -0300

    prontooooo

--- script.js
+++ script.js
@@ -2,30 +2,69 @@ const selecionado = document.getElementById('black');
 if (selecionado.classList) selecionado.classList.add ('selected');
 else selecionado.className += ' selected'
 
-let cores = document.querySelectorAll('.color');
-        for (let i = 0; i < cores.length; i += 1) {
-            cores[i].addEventListener('click', mudaClasse);
-    }
 
-    function mudaClasse (event) {
+let cores = document.querySelectorAll('.color');
+for (let i = 0; i < cores.length; i += 1) {
+        cores[i].addEventListener('click', mudaClasse);
+}
+function mudaClasse (event) {
     let corSelecionada = document.querySelector('.selected');
     corSelecionada.classList.remove('selected');
     event.target.classList.add('selected');
+}
+let pixels = document.getElementsByClassName('pixel')
+    for (let pixel of pixels){
+        pixel.addEventListener('click', mudaCor);
+    } 
+function mudaCor (event) {
+
+    const element = document.querySelector('.selected')
+    const cssObj = window.getComputedStyle(element);
+    let bgColor = cssObj.getPropertyValue('background-color')
+    event.target.style.backgroundColor = bgColor;
+}    
 
 
+function limpa () {
+    let botao = document.getElementById('clear-board');
+    botao.addEventListener ('click', function(){
+    let quadradinhos = document.getElementsByClassName('pixel');
+    for (let pixel of quadradinhos) {
+        pixel.style.backgroundColor = 'white'
+    }
+    })
+}
+
+limpa()
+
+/*let pixels = document.querySelectorAll('.pixel')  
+let pixels = document.getElementsByClassName('pixel');
+let cssObj = window.getComputedStyle(pixels, null);
+let bgColor = cssObj.getPropertyValue
+('background-color');
     
+for (let pixel of pixels) {
+        pixel.addEventListener('click', function(event){
+            let pintar = document.querySelector('.selected')
+            console.log(pintar.id)
+            event.target.
+    )}
+}
+
+function mudaCor(){ 
+    let pixels = document.querySelector('#pixel-board');
+    pixels.addEventListener('click', function(event) {
+    event.target.style.backgroundcolor = pixels.style.backgroundcolor
+})
 }
 
 
-/*
-function mudaCor (event) {
-    let pixelSelecionado = document.querySelector('.pixel')
-    event.target.classList.add('selected');
-
-
-let pixels = document.querySelectorAll('.pixel')
-    for (i in pixels)
-    pixelsaddEventListener('click', mudaCor)
-
-
-}*/
+ /*for (i = 0; i < pixels.length; i += 1) {
+    let pixelSelecionado = pixels[i];
+    let corSelecionada = document.querySelector('.selected');
+    pixelSelecionado.addEventListener('click', function(event){
+        event.target.style.backgroundcolor = corSelecionada.syle.backgroundcolor
+    })
+ }
+}
+*/
